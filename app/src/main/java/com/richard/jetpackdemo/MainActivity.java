44c555af9bed1943805aab01b1b364259e79fb59
Richard <819941971@qq.com>
2023-07-26 11:57:28 +0800

    MutableLiveData usage

--- app/src/main/java/com/richard/jetpackdemo/MainActivity.java
+++ app/src/main/java/com/richard/jetpackdemo/MainActivity.java
@@ -1,14 +1,29 @@
 package com.richard.jetpackdemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MutableLiveData<String> mutableLiveData = new MutableLiveData<>();
+        mutableLiveData.observe(this, new Observer<String>() {//1
+            @Override
+            public void onChanged(@Nullable final String s) {
+                Log.d(TAG, "onChanged, " + s);
+            }
+        });
+        mutableLiveData.postValue("Android Jetpack MutableLiveData demo");//2
     }
 }
