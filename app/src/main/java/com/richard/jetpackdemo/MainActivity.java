9291c72bbd1e1ba106d30c3df395dc2112e2c4f8
Richard <819941971@qq.com>
2023-07-26 12:08:29 +0800

    Transformations.map

--- app/src/main/java/com/richard/jetpackdemo/MainActivity.java
+++ app/src/main/java/com/richard/jetpackdemo/MainActivity.java
@@ -2,8 +2,11 @@ package com.richard.jetpackdemo;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.Transformations;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // demo 1
         MutableLiveData<String> mutableLiveData = new MutableLiveData<>();
         mutableLiveData.observe(this, new Observer<String>() {//1
             @Override
@@ -25,5 +29,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mutableLiveData.postValue("Android Jetpack MutableLiveData demo");//2
+
+        // demo 2
+        mutableLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String s) {
+                Log.d(TAG, "onChanged1, " + s);
+            }
+        });
+
+        // use Transformations.map to change value
+        LiveData transformedLiveData = Transformations.map(mutableLiveData, new Function<String, Object>() {
+            @Override
+            public Object apply(String name) {
+                return name + " + Android Richard step 1";
+            }
+        });
+
+        transformedLiveData.observe(this, new Observer() {
+            @Override
+            public void onChanged(@Nullable Object o) {
+                Log.d(TAG, "onChanged2, " + o.toString());
+            }
+        });
+        mutableLiveData.postValue("Android Richard step 2");
+
+        // output
+        // onChanged, Android Richard step 2
+        // onChanged1, Android Richard step 2
+        // onChanged2, Android Richard step 2 + Android Richard step 1
     }
 }
